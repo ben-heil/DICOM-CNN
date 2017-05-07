8375c01627bfa7a4414ea1554c050bd08e7d7393
Ben <ben_heil@yahoo.com>
2017-05-07 14:01:41 -0500

    Changed the model to use single images instead of 3d scans because of hardware limitations

--- CNN.py
+++ CNN.py
@@ -1,332 +1,96 @@
+# file: CNN.py
+# description: Implements a CNN that can be used to analyze DICOM images
+# author: Ben Heil
+# date: 4/27/17
 
-from __future__ import division
+import matplotlib.pyplot as plt
+import argparse
 import math
 import random
-import matplotlib
-import matplotlib.pyplot as plt
+import keras
+import sys
 import numpy
-import dicom
-import os 
-import theano.tensor as T
-import theano.tensor.nnet as nnet
-import theano.tensor.signal.pool as pool
-import theano
-import pickle
+from utilityFunctions import imageCount, readScan, readValidationImages, readBatch, readTest
+from keras.models import Sequential
+from keras.layers import Dense, convolutional, Activation, Flatten, pooling 
+from keras import optimizers
+from keras.layers.normalization import BatchNormalization
+import keras.backend as K
 
-#This function creates a dictionary that maps patient IDs to cancer incidence
-def parseLabels(labelFile):
-    #Throw away header
-    labelFile.readline()
-    patientLabels = {}
-    
-    #Read each line and add it to the dictionary
-    for line in labelFile:
-        line = line.strip().split(',')
-        #print(line)
-        #Throw out malformed input and empty lines
-        if len(line) != 2:
-            continue
-        patientLabels[line[0]] = line[1]
-    return patientLabels
+def main():
+    parser = argparse.ArgumentParser(description = "Convolutional Neural Net")
+    parser.add_argument("mode", help = "Specify whether to train or test the model")
+    args = parser.parse_args()
 
-def imageCount(dirName):
-    dirs = os.walk("./" + dirName)
-    #Skip the directory itself, only look at subdirectories
-    return len([dir for dir in dirs]) - 1
+    #Define CNN architecture
 
-def readScan(scanNum, dirName):
-    print("Reading " + str(scanNum))
-    dirGen = os.walk("./" + dirName)
-    dirs = [dir for dir in dirGen]
-    labelFile = open("./labels.csv")
-    idLabels = parseLabels(labelFile)
-    
-    patientData = []
-    fileCount = 0
-    currDir = os.path.basename(dirs[scanNum][0])
-    dataList = []
-    #Add each array to a list
-    for image in dirs[scanNum][2]:
-        imageHandle = dicom.read_file(dirs[scanNum][0] + '/' + image)
-        imgData = imageHandle.pixel_array
-        dataList.append(imageHandle.pixel_array)
-            
-        
-        #Add the 3d array of all images from a patient to the list
-        patientData = numpy.stack(dataList).astype(float)
-    patientData -= numpy.mean(patientData)
-    label = -1
+    #Load model if it has a savefile, otherwise reinitialize
     try:
-        label = idLabels[currDir]
+        print("Loading model")
+        model = keras.models.load_model("./CNN.save")
     except:
-        label = -1
-    return patientData, label 
+        print("save file not found, reinstantiating...")
+        #Initialize a model with five convolutional layers and one hidden layer
+        model = Sequential([
+            convolutional.Conv2D(16, 15, 
+            input_shape = (512,512,1)),
+            pooling.MaxPooling2D(pool_size = (4)),
+            convolutional.Conv2D(16, 7),
+            Activation('tanh'),
+            BatchNormalization(),
+            convolutional.Conv2D(16, 7),
+            Activation('tanh'),
+            BatchNormalization(),
+            convolutional.Conv2D(16, 7),
+            Activation('tanh'),
+            BatchNormalization(),
+            pooling.MaxPooling2D(pool_size = (4)),
+            convolutional.Conv2D(16, 3),
+            Activation('tanh'),
+            BatchNormalization(),
+            pooling.MaxPooling2D(pool_size = (4)),
+        Flatten(),
+            Dense(16, activation = "tanh"),
+        BatchNormalization(),
+            Dense(1, activation = "tanh")])
 
-def readImages():
-    labelFile = open("./labels.csv")
-    idLabels = parseLabels(labelFile)
-    dirs = os.walk("./images")
-    #Skip the directory itself, only look at subdirectories
-    dirs.next()
-    
-    #Create a list of tuples mapping 3d numpy arrays of image data to their label
-    labels = []
-    patientData = []
-    fileCount = 0
-    for directory in dirs:
-        fileCount += 1
-        if fileCount %30 == 1:
-            print("Read " + str(fileCount) + " files")
-        currDir = os.path.basename(directory[0])
-        if currDir in idLabels:
-            dataList = []
-            #Add each array to a list
-            for image in directory[2]:
-                imageHandle = dicom.read_file(directory[0] + '/' + image)
-                imgData = imageHandle.pixel_array
-                dataList.append(imageHandle.pixel_array)
-            
-            #Preprocess all images from the patient to set a zero mean
-            dataList -= numpy.mean(dataList)
+    #Compile the model and use stochastic gradient descent to minimize error
+    model.compile(loss = 'mean_squared_error', 
+    optimizer = optimizers.SGD(lr = .01), metrics = ['acc'])  
+
+
+    #Create a callback function that logs the results of training 
+    log = keras.callbacks.CSVLogger("KerasLog.csv", append = True)
+    #END OF ARCHITECTURE
+
+    if args.mode.lower() == "test":
+        print("Beginning test")
+        testImages, testLabels = readTest()
+        print(model.evaluate(testImages.reshape(40,512,512,1), testLabels))
+
+    elif args.mode.lower() == "train":
+        print("Reading validation images")
+
+        batchSize = 2 
+        valImages, valLabels = readValidationImages()
         
-            #Add the 3d array of all images from a patient to the list
-            patientData.append(numpy.stack(dataList))
-            labels.append(idLabels[currDir])
-    return patientData, labels
+        bestLoss = float('inf')
+        for i in range(1000):
+            batch, labels = readBatch(batchSize)
+
+            history = model.fit(x = batch.reshape(batchSize,512,512,1), 
+            y = labels, epochs = 5,
+            validation_data = (valImages.reshape(16,512,512,1), valLabels),
+            callbacks = [log])
+
+            #Check to see whether the most recent round of training led to an
+            #improved accuracy rate
+            currLoss = history.history['val_loss'][-1]
+            if currLoss < bestLoss:
+                print("Saving model with loss of " + str(currLoss))
+                model.save("./CNN.save")
 
 
-def readValidationImages():
-    labelFile = open("./labels.csv")
-    idLabels = parseLabels(labelFile)
-    dirs = os.walk("./validation")
-    #Skip the directory itself, only look at subdirectories
-    dirs.next()
-    
-    labels = []
-    patientData = []
-    for directory in dirs:
-        currDir = os.path.basename(directory[0])
-	if currDir in idLabels:
-            dataList = []
-            #Add each array to a list
-            for image in directory[2]:
-                if image == "desktop.ini":
-                    continue
-                imageHandle = dicom.read_file(directory[0] + '/' + image)
-                imgData = imageHandle.pixel_array.astype(float)
-                dataList.append(imageHandle.pixel_array)
-            
-            #Preprocess all images from the patient to set a zero mean
-            dataList -= numpy.mean(dataList)
-        
-            #Add the 3d array of all images from a patient to the list
-            patientData.append(numpy.stack(dataList))
-            labels.append(idLabels[currDir])
-    return patientData, labels
+if __name__ == "__main__":
+    main()
 
-
-
-Img = T.tensor4(name="Img")
-Lab = T.dscalar()
-
-#Layer 1
-print("Compiling layer 1")
-f1size = 8
-numFilters1 = 5
-p1Factor = 3
-learnVal = numpy.array(.01)
-learnRate = theano.shared(learnVal, 'learnRate')
-#Load the shared variables if possible, otherwise initialize them
-try:
-    f1File = open("F1_vanilla.save" , "rb")
-    F1 = pickle.load(f1File)
-    f1File.close()
-    b1File = open("b1_vanilla.save", "rb")
-    b1 = pickle.load(f1File)
-    b1File.close()
-except:
-    f1Arr = numpy.random.randn(numFilters1, 1, f1size ,f1size) 
-    F1 = theano.shared(f1Arr, name = "F1")
-    bias1 = numpy.random.randn()
-    b1 = theano.shared(bias1, name = "b1")
-
-#Output = batches x channels x 512 - f1size x 512 - f1size
-conv1 = nnet.sigmoid(nnet.conv2d(Img, F1) + b1)
-pool1 = pool.pool_2d(conv1, (p1Factor,p1Factor), ignore_border = True)
-layer1 = theano.function([Img], pool1)
-
-#Layer 2
-print("Compiling layer 2")
-f2size = 7
-numFilters2 = 10
-pool2Factor = 6
-try:
-    f2File = open("F2_vanilla.save", "rb")
-    F2 = pickle.load(f2File)
-    f2File.close()
-    b2File = open("b2_vanilla.save", "rb")
-    b2 = pickle.load(b2File)
-    b2File.close()
-except:
-    f2Arr = numpy.random.randn(numFilters2, numFilters1, f2size, f2size)
-    F2 = theano.shared(f2Arr, name = "F2")
-    bias2 = numpy.random.randn()
-    b2 = theano.shared(bias2, name = "b2")
-conv2 = nnet.sigmoid(nnet.conv2d(pool1, F2) + b2)
-pool2 = pool.pool_2d(conv2, (pool2Factor,pool2Factor), ignore_border = True)
-layer2 = theano.function([Img], pool2)
-
-#Calculate the size of the output of the second convolutional layer
-convOutLen = (((512 - numFilters1) //p1Factor + 1) - numFilters2) // pool2Factor + 1
-convOutLen = convOutLen * convOutLen * numFilters2
-
-#Layer 3
-print("Compiling layer 3")
-try:
-    b3File = open("b3_vanilla.save", "rb")
-    b3 = pickle.load(b3File)
-    b3File.close()
-    w3File = open("w3_vanilla.save", "rb")
-    w3 = pickle.load(w3File)
-    w3File.close()
-except:
-    b3arr = numpy.random.randn()
-    b3 = theano.shared(b3arr, name = "b3")
-    w3arr = numpy.random.randn(convOutLen, convOutLen // numFilters2)
-    w3 = theano.shared(w3arr, name = "w3")
-hidden3 = theano.dot(pool2.flatten(), w3) + b3
-layer3 = theano.function([Img], hidden3)
-
-#Layer 4
-print("Compiling layer 4")
-try:
-    w4File = open("w4_vanilla.save", "rb")
-    w4 = pickle.load(w4File)
-    w4File.close()
-    b4File = open("b4_vanilla.save", "rb")
-    b4 = pickle.load(b4File)
-    b4File.close()
-except:
-    w4arr = numpy.random.randn(convOutLen // numFilters2)
-    w4 = theano.shared(w4arr, name = "w4")
-    b4arr = numpy.random.randn()
-    b4 = theano.shared(b4arr, name = "b4")
-hidden4In = nnet.sigmoid(hidden3)
-hidden4 = theano.dot(hidden4In, w4) + b4
-layer4 = theano.function([Img], hidden4)
-
-#Output layer
-print("Compiling training and validation functions")
-output = nnet.sigmoid(hidden4)
-
-error = T.sqr(abs(output - Lab))
-F1Grad = T.grad(error, F1)
-F2Grad = T.grad(error, F2)
-w3Grad = T.grad(error, w3)
-w4Grad = T.grad(error, w4)
-b1Grad = T.grad(error, b1)
-b2Grad = T.grad(error, b2)
-b3Grad = T.grad(error, b3)
-b4Grad = T.grad(error, b4)
-
-validate = theano.function([Img, Lab], error)
-train = theano.function([Img, Lab], error, updates = [(F1, F1 - F1Grad * learnRate),
-         (F2, F2 - F2Grad * learnRate),
-         (w3, w3 - w3Grad * learnRate),
-         (w4, w4 - w4Grad * learnRate),
-         (b1, b1 - b1Grad * learnRate),
-         (b2, b2 - b2Grad * learnRate),
-         (b3, b3 - b3Grad * learnRate),
-         (b4, b4 - b4Grad * learnRate)])
-
-#END OF ARCHITECTURE
-
-print("Reading validation images")
-valImages, valLabels = readValidationImages()
-besterr = float("inf")
-posPatientCount = imageCount("posTrain")
-negPatientCount = imageCount("negTrain")
-
-logFile = open("cnnLog.txt", "w")
-
-for i in range(10000):
-    print(i)
-    patientNum = int(math.floor(random.random() * posPatientCount ) + 1)
-    posPatientData, label = readScan(patientNum, "posTrain")
-    patientNum = int(math.floor(random.random() * negPatientCount ) + 1)
-    negPatientData, negLabel = readScan(patientNum, "negTrain")
-    if label == -1:
-        continue
-
-    sumErr = 0
-    for j in range(posPatientData.shape[0]):
-        image = posPatientData[j]
-        sumErr += train(image.reshape(1,1,512,512), int(label))
-    logFile.write("Pos err = " + str(sumErr))
-    sumErr = 0
-    for j in range(negPatientData.shape[0]):
-	image = negPatientData[j]
-        sumErr += train(image.reshape(1,1,512,512), int(negLabel))
-    logFile.write("\tNeg err = " + str(sumErr) + "\n")
-
-    #Use validation set to test error
-    if i%5 == 0:
-        currErr = 0
-
-        for j in range(len(valImages)):
-            label = valLabels[j]
-            for k in range(valImages[j].shape[0]):
-                image = valImages[j][k]
-                currErr += validate(image.reshape(1,1,512,512), int(label))
-
-        print("Validation err = " + str(currErr))
-        if  currErr < besterr:
-            besterr = currErr
-            print("Saving weight data")
-            iterationFile = open("bestErrorIteration.txt", "w+")
-            iterationFile.write(str(i) + "\n" + str(bestErr))
-            iterationFile.close()
-            try:
-                f1File = open("F1_vanilla.save" , "wb")
-                b1File = open("b1_vanilla.save" , "wb")
-                f2File = open("F2_vanilla.save", "wb")
-                b2File = open("b2_vanilla.save", "wb")
-                w3File = open("w3_vanilla.save", "wb")
-                b3File = open("b3_vanilla.save", "wb")
-                w4File = open("w4_vanilla.save", "wb")
-                b4File = open("b4_vanilla.save", "wb")
-    
-                pickle.dump(F1, f1File)
-                pickle.dump(b1, b1File)
-                pickle.dump(F2, f2File)
-                pickle.dump(b2, b2File)
-                pickle.dump(w3, w3File)
-                pickle.dump(b3, b3File)
-                pickle.dump(w4, w4File)
-                pickle.dump(b4, b4File)
-            
-                f1File.close()
-                b1File.close()
-                f2File.close()
-                b2File.close()
-                w3File.close()
-                b3File.close()
-                w4File.close()
-                b4File.close()
-
-            except:
-                print("Error dumping weight data")
-
-#result = layer1(patientData[0][0].reshape(1,1,512,512))
-#plt.subplot(1,3,1)
-#plt.imshow(patientData[0][1])
-#plt.gray()
-#plt.subplot(1,3,2)
-#plt.imshow(result[0][0])
-#plt.gray()
-#result = layer2(patientData[0][0].reshape(1,1,512,512))
-#print(result[0][0].shape)
-#plt.subplot(1,3,3)
-#plt.imshow(result[0][0])
-#plt.gray()
-#plt.show()
--- utilityFunctions.py
+++ utilityFunctions.py
@@ -1,5 +1,6 @@
 #File containing functions used across different CNN architectures
 
+from skimage import exposure
 import os
 import dicom
 import math
@@ -8,132 +9,127 @@ import numpy
 import matplotlib.pyplot as plt
 import time
 
-def parseLabels(labelFile):
-    #Throw away header
-    labelFile.readline()
-    patientLabels = {}
-
-    #Read each line and add it to the dictionary
-    for line in labelFile:
-        line = line.strip().split(',')
-        #print(line)
-        #Throw out malformed input and empty lines
-        if len(line) != 2:
-            continue
-        patientLabels[line[0]] = line[1]
-    return patientLabels
-
-def maxPool(images, endSize):
-    totalImages = len(images)
-    poolSize = totalImages // endSize
-    extras = totalImages % endSize
-    if extras > 0:
-        extraIteration = totalImages // extras
-    else:
-        extraIteration = 0
-    extraCount = 0
-    i = 0
-    iteration = 0
-    output = numpy.zeros((endSize,64,64))
-
-    while iteration < endSize:
-    #while i < images.shape[0]:
-        size = poolSize
-        if iteration % (extraIteration - 1) == 0 and extraCount < extras:
-            extraCount += 1
-            size += 1
-
-        choice = i + int(math.floor(random.random() * size))
-        #print(output.shape)
-        #print(images.shape)
-        #print(iteration)
-        #print(i)
-        output[iteration] = images[choice]
-
-        i += size
-        iteration += 1
-    return output
-
 def imageCount(dirName):
-    dirs = os.walk("./" + dirName)
-    #Skip the directory itself, only look at subdirectories
-    return len([dir for dir in dirs]) - 1
+    
+    for root, dir, files in os.walk("./" + dirName):
+        scans = [file  for file in files if file != "desktop.ini"]
+        return len(scans)
+    print(dirName + " is not a valid directory")
+    return -1
 
 def readScan(scanNum, dirName):
-    print("Reading image " + str(scanNum) + " from " + dirName)
-    dirGen = os.walk("./" + dirName)
-    dirs = [dir for dir in dirGen]
-    labelFile = open("./labels.csv")
-    idLabels = parseLabels(labelFile)
-
-    patientData = []
-    currDir = os.path.basename(dirs[scanNum][0])
-    images = []
-    #Add each array to a list
-    for image in dirs[scanNum][2]:
-	if image != "desktop.ini":
-            imageData = numpy.load(dirs[scanNum][0] + '/' + image)
-	    images.append(imageData)
     
-    #Add the 3d array of all images from a patient to the list
-    patientData = numpy.stack(images).astype(float)
-    patientData -= patientData.mean()
-    patientData = patientData / patientData.max()
-    label = -1
-    try:
-        label = idLabels[currDir]
-    except:
-        label = -1
-    return patientData, label
+    for root, dir, files in os.walk("./" + dirName):
+        scans = [file  for file in files if file != "desktop.ini"]
+        print("Reading image " + scans[scanNum] + " from " + dirName)
+        data = numpy.load("./" + dirName + "/" + scans[scanNum])
+        #plt.imshow(data)
+        #plt.show()
+        data = exposure.equalize_adapthist(data)        
+        #plt.imshow(data)
+        #plt.show()
+        return data
+    print(dirName + " is not a valid directory")
+    return -1
     
-
-def readDicomScan(scanNum, dirName):
-    print("Reading image " + str(scanNum) + " from " + dirName)
-    dirGen = os.walk("./" + dirName)
-    dirs = [dir for dir in dirGen]
-    labelFile = open("./labels.csv")
-    idLabels = parseLabels(labelFile)
-
-    patientData = []
-    currDir = os.path.basename(dirs[scanNum][0])
-    dataList = []
-    images = []
-    #Add each array to a list
-    for image in dirs[scanNum][2]:
-        imageHandle = dicom.read_file(dirs[scanNum][0] + '/' + image)
-	images.append(imageHandle)
-    
-    images.sort(key=lambda image: image.ImagePositionPatient)
-    for image in images:
-	plt.imshow(image.pixel_array)
-	plt.draw()
-	plt.pause(.01)
-	print("Figure")
-        dataList.append(image.pixel_array)
-    #Add the 3d array of all images from a patient to the list
-    patientData = numpy.stack(dataList).astype(float)
-    patientData = patientData / patientData.max()
-    patientData -= numpy.mean(patientData)
-    label = -1
-    try:
-        label = idLabels[currDir]
-    except:
-        label = -1
-    return patientData, label
-
 def readValidationImages():
-    labelFile = open("./labels.csv")
-    idLabels = parseLabels(labelFile)
-    dirs = os.walk("./validation")
-    #Skip the directory itself, only look at subdirectories
-    dirs.next()
-
     labels = []
     patientData = []
     
-    for i in range(1,imageCount("validation") + 1):
-        label, data = readScan(i, "validation")
-	labels.append(label)
-	patientData.append(data)
+    for i in range(0,imageCount("tumorPosVal")):
+        data = readScan(i, "tumorPosVal")
+        labels.append(1)
+        patientData.append(data)
+    for i in range(0,imageCount("tumorNegVal")):
+        data = readScan(i, "tumorNegVal")
+        labels.append(0)
+        patientData.append(data)
 
-    return patientData, labels
+    patientData = numpy.stack(patientData).astype(float)
+
+    return patientData, numpy.stack(labels)
+
+def readBatch(batchSize):
+    labels = []
+    patientData = []
+    
+    posLen = imageCount("tumorPosTrain")
+    negLen = imageCount("tumorNegTrain")
+
+    posStart = random.randint(0,(posLen- (batchSize//2)) - 1)
+    negStart = random.randint(0,(negLen- (batchSize//2)) - 1)
+
+    for i in range(batchSize //2):
+        data = readScan(posStart + i, "tumorPosTrain")
+        labels.append(1)
+        patientData.append(data)
+        data = readScan(negStart + i, "tumorNegTrain")
+        labels.append(0)
+        patientData.append(data)
+    
+    patientData = numpy.stack(patientData).astype(float)
+    return patientData, numpy.stack(labels)
+
+def readAll():
+    labels = []
+    patientData = []
+    
+    posLen = imageCount("tumorPosTrain")
+    negLen = imageCount("tumorNegTrain")
+
+    for i in range(posLen):
+        data = readScan(i, "tumorPosTrain")
+        labels.append(1)
+        patientData.append(data)
+    for i in range(negLen):
+        data = readScan(i, "tumorNegTrain")
+        labels.append(0)
+        patientData.append(data)
+    
+    patientData = numpy.stack(patientData).astype(float)
+    patientData -= patientData.mean()
+    patientData = patientData / patientData.max()
+    return patientData, numpy.stack(labels)
+
+def normReadAll():
+    labels = []
+    patientData = []
+    
+    posLen = imageCount("tumorPosTrain")
+    negLen = imageCount("tumorNegTrain")
+
+    for i in range(posLen):
+        data = readScan(i, "tumorPosTrain")
+        labels.append(1)
+        data = exposure.equalize_adapthist(data)        
+        patientData.append(data)
+    for i in range(negLen):
+        data = readScan(i, "tumorNegTrain")
+        labels.append(0)
+        data = exposure.equalize_adapthist(data)        
+        patientData.append(data)
+    
+    patientData = numpy.stack(patientData).astype(float)
+    return patientData, numpy.stack(labels)
+
+def readTest():
+    labels = []
+    patientData = []
+    
+    posLen = imageCount("tumorPosTest")
+    negLen = imageCount("tumorNegTest")
+
+    for i in range(posLen):
+        data = readScan(i, "tumorPosTest")
+        labels.append(1)
+        data = exposure.equalize_adapthist(data)        
+        patientData.append(data)
+    for i in range(negLen):
+        data = readScan(i, "tumorNegTest")
+        labels.append(0)
+        data = exposure.equalize_adapthist(data)        
+        patientData.append(data)
+    
+    patientData = numpy.stack(patientData).astype(float)
+    return patientData, numpy.stack(labels)
+    
